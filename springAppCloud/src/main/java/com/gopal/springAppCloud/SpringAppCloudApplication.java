920c1a354986aedc66f3fc11ef199b64e10358ff
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-02-27 13:53:14 +0530

    spring web app

---
+++ springAppCloud/src/main/java/com/gopal/springAppCloud/SpringAppCloudApplication.java
@@ -0,0 +1,13 @@
+package com.gopal.springAppCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppCloudApplication.class, args);
+	}
+
+}
---
+++ springAppCloud/src/test/java/com/gopal/springAppCloud/SpringAppCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopal.springAppCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
